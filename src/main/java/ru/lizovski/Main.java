24752783f220e6c7d797bd50e4a3239b403c10db
EIIIE <andrey2080@mail.ru>
2018-04-09 02:18:59 +0300

    Добавлена инфа о юзере

---
+++ src/main/java/ru/lizovski/Main.java
@@ -0,0 +1,23 @@
+package ru.lizovski;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class Main {
+    public static void main(String[] args) {
+        // Initialize Api Context
+        ApiContextInitializer.init();
+
+        // Instantiate Telegram Bots API
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        // Register our bot
+        try {
+            botsApi.registerBot(new SugarCloudBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+        System.out.println("SugarCloudBot started!");
+    }
+}
--- src/main/java/ru/lizovski/SugarCloudBot.java
+++ src/main/java/ru/lizovski/SugarCloudBot.java
@@ -1,13 +1,13 @@
 package ru.lizovski;
 
-import org.telegram.telegrambots.ApiContextInitializer;
-import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
-import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class SugarCloudBot extends TelegramLongPollingBot {
@@ -15,34 +15,33 @@ public class SugarCloudBot extends TelegramLongPollingBot {
     private static String botToken = System.getenv("botToken");
     private static String botUsername= System.getenv("botUsername");
 
-    public static void main(String[] args) /*throws IOException*/ {
-
-        ApiContextInitializer.init();
-        TelegramBotsApi botsApi = new TelegramBotsApi();
-
-        try {
-            botsApi.registerBot(new SugarCloudBot());
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public String getBotUsername() {
-        return botUsername;
-    }
-
-
+    @Override
     public void onUpdateReceived(Update update) {
         // We check if the update has a message and the message has text
         if (update.hasMessage() && update.getMessage().hasText()) {
-            SendMessage message = new SendMessage()
-                    .setChatId(update.getMessage().getChatId());
+
+            // Set variables
+            String user_first_name = update.getMessage().getChat().getFirstName();
+            String user_last_name = update.getMessage().getChat().getLastName();
+            String user_username = update.getMessage().getChat().getUserName();
+            long user_id = update.getMessage().getChat().getId();
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+            String answer = message_text;
+
             if (update.getMessage().getText().equals("/start")) {
-                message.setText("Hello, world! This is simple bot!");
+                answer = "Hello, world! This is simple bot!";
             }
             if (update.getMessage().getText().equals("/me")) {
-                message.setText("Just a nice try in bots world...");
+                answer = "Just a nice try in bots world...";
             }
+
+            SendMessage message = new SendMessage() // Create a message object object
+                    .setChatId(chat_id)
+                    .setText(answer);
+
+            log(user_username, user_first_name, user_last_name, Long.toString(user_id), message_text, answer);
+
             try {
                 System.out.println(message);
                 execute(message); // Call method to send the message
@@ -50,6 +49,20 @@ public class SugarCloudBot extends TelegramLongPollingBot {
                 e.printStackTrace();
             }
         }
+    }
+
+    private void log(String username, String first_name, String last_name, String user_id, String txt, String bot_answer) {
+        System.out.println("\n ----------------------------");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        System.out.println(dateFormat.format(date));
+        System.out.println("Message from "+ username + " " + first_name + " " + last_name + ". (id = " + user_id + ") \n Text - " + txt);
+        System.out.println("Bot answer: \n Text - " + bot_answer);
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
     }
 
     @Override
